fb0dd41688a21080531ff85f5625c2430dc45fb5
Frank Nielson <frank.nielson@stgconsulting.com>
2026-07-07 19:27:55 -0600

    Updating to use ATtiny1624 vs wemos

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,24 @@
 #include <Arduino.h>
 
 
+#ifdef ATtiny
+constexpr uint8_t PIN_LEFT_LEDS = PIN_PB0;
+#else 
+constexpr uint8_t PIN_TRIGGER = D2;
+constexpr uint8_t PIN_LEFT_LEDS = D8;
+constexpr uint8_t PIN_RIGHT_LEDS = D7;
+constexpr uint8_t PIN_FRONT_LEDS = D5;
+constexpr uint8_t PIN_SPEAKER = D1;
+#endif 
+
+// Phaser sound: a fast, repeating descending frequency sweep.
+constexpr unsigned long phaserSweepMs = 220;
+constexpr int phaserHighFreq = 2200;
+constexpr int phaserLowFreq = 500;
+int lastPhaserFreq = -1;
+
 
 #ifdef ATtiny
-#define LEFT_LEDS_PIN PIN_PB0
-#else 
-#define LEFT_LEDS_PIN D8
-#endif 
-// const int triggerPin = D2;
-// const int leftLedsPin = D8;
-// const int rightLedsPin = D7;
-// const int frontLedsPin = D5;
-// const int speakerPin = D1;
-
-// // Phaser sound: a fast, repeating descending frequency sweep.
-// const unsigned long phaserSweepMs = 220;
-// const int phaserHighFreq = 2200;
-// const int phaserLowFreq = 500;
-// int lastPhaserFreq = -1;
-
-// void setup() {
-//   pinMode(triggerPin, INPUT_PULLUP);
-//   pinMode(leftLedsPin, OUTPUT);
-//   pinMode(rightLedsPin, OUTPUT);
-//   pinMode(frontLedsPin, OUTPUT);
-//   pinMode(speakerPin, OUTPUT);
-// }
-
-// void loop() {
-//   int triggerState = digitalRead(triggerPin);
-
-//   if (triggerState == LOW) {
-//     digitalWrite(leftLedsPin, HIGH);
-//     digitalWrite(rightLedsPin, HIGH);
-//     digitalWrite(frontLedsPin, HIGH);
-
-//     // Sweep the tone from high to low every phaserSweepMs, looping
-//     // continuously for a classic "pew" phaser sound while held.
-//     unsigned long elapsed = millis() % phaserSweepMs;
-//     int freq = phaserHighFreq - (int)((phaserHighFreq - phaserLowFreq) * elapsed / phaserSweepMs);
-//     if (freq != lastPhaserFreq) {
-//       tone(speakerPin, freq);
-//       lastPhaserFreq = freq;
-//     }
-//   }
-//   else {
-//     digitalWrite(leftLedsPin, LOW);
-//     digitalWrite(rightLedsPin, LOW);
-//     digitalWrite(frontLedsPin, LOW);
-//     noTone(speakerPin);
-//     lastPhaserFreq = -1;     
-//   }
-// }
-
 void setup()
 {
   // initialize LED digital pin as an output.
@@ -70,3 +36,38 @@ void loop()
    // wait for a second
   delay(500);
 }
+#else
+void setup() {
+  pinMode(PIN_TRIGGER, INPUT_PULLUP);
+  pinMode(PIN_LEFT_LEDS, OUTPUT);
+  pinMode(PIN_RIGHT_LEDS, OUTPUT);
+  pinMode(PIN_FRONT_LEDS, OUTPUT);
+  pinMode(PIN_SPEAKER, OUTPUT);
+}
+
+void loop() {
+  int triggerState = digitalRead(PIN_TRIGGER);
+
+  if (triggerState == LOW) {
+    digitalWrite(PIN_LEFT_LEDS, HIGH);
+    digitalWrite(PIN_RIGHT_LEDS, HIGH);
+    digitalWrite(PIN_FRONT_LEDS, HIGH);
+
+    // Sweep the tone from high to low every phaserSweepMs, looping
+    // continuously for a classic "pew" phaser sound while held.
+    unsigned long elapsed = millis() % phaserSweepMs;
+    int freq = phaserHighFreq - (int)((phaserHighFreq - phaserLowFreq) * elapsed / phaserSweepMs);
+    if (freq != lastPhaserFreq) {
+      tone(PIN_SPEAKER, freq);
+      lastPhaserFreq = freq;
+    }
+  }
+  else {
+    digitalWrite(PIN_LEFT_LEDS, LOW);
+    digitalWrite(PIN_RIGHT_LEDS, LOW);
+    digitalWrite(PIN_FRONT_LEDS, LOW);
+    noTone(PIN_SPEAKER);
+    lastPhaserFreq = -1;     
+  }
+}
+#endif
